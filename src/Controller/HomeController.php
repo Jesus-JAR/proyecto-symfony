a127cfc9b31305b9d9ebdba164bebcb0af7fa8ba
Jesus Arraras <jesusarraras1@gmail.com>
2023-03-22 14:20:03 +0100

    animales

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+# use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    /**
+     * #[Route('/home', name: 'app_home')]
+    */
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
@@ -16,5 +19,24 @@ class HomeController extends AbstractController
             'hello' => 'Hola me llamo Jesus bienvenido al curso de Symfony 6'
         ]);
     }
+    
+    public function animales(string $name = null): Response
+    {
+        /**
+         * if ($name == null){
+           * $name = "Bolt";
+        
+         */
+       # $name = u(str_replace("-", " ", $name))->title(true);
+       $name = str_replace("-", " ", $name);
 
+
+
+        $title = "Bienvenido a la pagina de Animales";
+
+        return $this->render('home/animales.html.twig', [
+            'title' => $title,
+            'name' => $name
+        ]);
+    }
 }
